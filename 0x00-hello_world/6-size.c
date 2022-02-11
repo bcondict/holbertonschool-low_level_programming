cf85df676dd8e9c94d9bf0190c5cd4cfd7aeb2a6
bcondict <jesus.junc10@gmail.com>
2022-02-10 21:04:16 -0800

    6-size.c dump mistake

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int l_l_intType;
 	float fType;
 
-	printf("Size of char: %lu byte(s)\n", sizeof(cType));
-	printf("Size of int: %lu byte(s)\n", sizeof(iType));
-	printf("Size of long int: %lu byte(s)s\n", sizeof(l_intType));
-	printf("Size of long long int: %lu byte(s)\n", sizeof(l_l_intType));
-	printf("Size of float: %lu byte(s)\n", sizeof(fType));
+	printf("Size of a char: %lu byte(s)\n", sizeof(cType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(iType));
+	printf("Size of a long int: %lu byte(s)s\n", sizeof(l_intType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(l_l_intType));
+	printf("Size of a float: %lu byte(s)\n", sizeof(fType));
 	return (0);
 }
 
