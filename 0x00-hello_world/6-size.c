b7d694e23051963c96b3a43033d025afb2768716
bcondict <jesus.junc10@gmail.com>
2022-02-10 18:45:57 -0800

    6-size.c without s

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	printf("Size of char: %zu bytes(s)\n", sizeof(iType));
-	printf("Size of int: %zu bytes(s)\n", sizeof(cType));
+	printf("Size of char: %zu byte(s)\n", sizeof(iType));
+	printf("Size of int: %zu byte(s)\n", sizeof(cType));
 	printf("Size of long int: %zu byte(s)s\n", sizeof(l_intType));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(l_l_intType));
 	printf("Size of float: %zu byte(s)\n", sizeof(fType));
