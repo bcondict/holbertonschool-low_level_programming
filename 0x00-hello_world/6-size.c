f3dcbd71533453080e94f62a5ce2e99841563b00
bcondict <jesus.junc10@gmail.com>
2022-02-10 19:06:35 -0800

    6-size.c summit error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of char: %zu byte(s)\n", sizeof(iType));
 	printf("Size of int: %zu byte(s)\n", sizeof(cType));
-	printf("Size of long int: zlu byte(s)s\n", sizeof(l_intType));
+	printf("Size of long int: %zu byte(s)s\n", sizeof(l_intType));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(l_l_intType));
 	printf("Size of float: %zu byte(s)\n", sizeof(fType));
 	return (0);
