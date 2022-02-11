626a06b45fc901e1889b00c7428347a51b8c3265
bcondict <jesus.junc10@gmail.com>
2022-02-11 15:47:21 -0500

    1-last_digit.c if{pf} eif{pf} elif{pf}

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - evatuates random number
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	lastDigit = n % 10;
+	if (lastDigit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, lastDigit)
+	}
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of %d is %d and is 0", n, lastDigit)
+	}
+	else if (lastDigit < 6, lastDigit != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0")
+	}
+	return (0);
+}
