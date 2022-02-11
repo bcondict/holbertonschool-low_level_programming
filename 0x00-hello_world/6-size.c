7f929d092af7e0ec3b8e1034e2555cfce212069b
bcondict <jesus.junc10@gmail.com>
2022-02-10 19:12:44 -0800

    6-size.c correct issue

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,8 @@ int main(void)
 	long long int l_l_intType;
 	float fType;
 
-	printf("Size of char: %zu byte(s)\n", sizeof(iType));
-	printf("Size of int: %zu byte(s)\n", sizeof(cType));
+	printf("Size of char: %zu byte(s)\n", sizeof(cType));
+	printf("Size of int: %zu byte(s)\n", sizeof(iType));
 	printf("Size of long int: %zu byte(s)s\n", sizeof(l_intType));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(l_l_intType));
 	printf("Size of float: %zu byte(s)\n", sizeof(fType));
