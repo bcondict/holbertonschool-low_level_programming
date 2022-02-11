ef32bda14dc6235810e3c262c6da352d85c8ddfe
bcondict <jesus.junc10@gmail.com>
2022-02-11 17:29:25 -0500

    3-print_alphabets.c betty corrections

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-
-int main (void)
+/**
+* main - sprint alphabet in lowercase
+* Return: Always 0
+*/
+int main(void)
 {
-    for (int x = 'a'; x <= 'z'; x++)
-    {
-        x = tolowe(x);
-        putchar(x)
-    }
-    return (0);
+	for (int x = 'a'; x <= 'z' ; x++)
+	{
+		x = tolowe(x);
+		putchar(x);
+	}
+	return (0);
 }
