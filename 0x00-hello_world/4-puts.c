19ba14d8329ad747ad9679373ebffd4de9c988c3
bcondict <jesus.junc10@gmail.com>
2022-02-10 16:27:15 -0800

    4-puts.c empty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,2 @@
 #!/bin/bash
 
-// C program to show the use of puts
-
-#include <stdio.h>
-
-int main()
-  
-{
-  
-  puts("Programming is like building a multilingual puzzle");
-  
-  getchar();
-  
-  return 0;
-  
-}
