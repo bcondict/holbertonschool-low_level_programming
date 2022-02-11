cd7747a07b70fb05e1420de343d238c4e49fba57
bcondict <jesus.junc10@gmail.com>
2022-02-10 17:14:45 -0800

    don't understand it'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
 
  * main - Entry point
@@ -13,11 +11,9 @@
  */
 
 int main(void)
-  
+
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
   
 }
