cb3849769e6ff824066240af07013b6a83f58a3a
bcondict <jesus.junc10@gmail.com>
2022-02-11 16:05:12 -0500

    1-last_digit.c if-eil

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,12 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
 	}
-	else if (lastDigit == 0)
+	if (lastDigit == 0)
 	{
 		printf("Last digit of %d is %d and is 0", n, lastDigit);
 	}
-	else if (lastDigit < 6 (!= 0))
+	if (lastDigit < 6)
+	else if (lastDigit != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, lastDigit);
 	}
