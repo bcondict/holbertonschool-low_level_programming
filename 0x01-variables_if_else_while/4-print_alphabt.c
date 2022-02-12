6ba4b6a309b9eeac11b1dc5f30a98fbbafaea6bc
bcondict <jesus.junc10@gmail.com>
2022-02-11 19:39:16 -0500

    4-print_alphabt.c if r0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - sprint alphabet in lowercase
+* Return: Always 0
+*/
+int main(void)
+{
+	char x;
+
+	if (x = e && x = q)
+		return (0);
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
