98d2328c197b58f124dae2a6630ad2a7aa36db7d
bcondict <jesus.junc10@gmail.com>
2022-02-11 22:40:42 -0500

    9-print_comb for pchr con pchr

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - sprint alphabet in lowercase
+* Return: Always 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+    {
+		putchar(x);	
+		if (x == '9')
+		continue;
+		putchar(',');
+    }
+
+	putchar('\n');
+	return (0);
+}
