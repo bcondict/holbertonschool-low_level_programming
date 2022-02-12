223fbaedbe79116d11db75b338cc49e86c0df4ee
bcondict <jesus.junc10@gmail.com>
2022-02-11 20:17:12 -0500

    5-print_numbers.c 0-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - sprint alphabet in lowercase
+* Return: Always 0
+*/
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '10'; x++)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
