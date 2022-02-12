d6500b46ca06ad0a83e8e8aad41d521a6e603abe
bcondict <jesus.junc10@gmail.com>
2022-02-11 22:45:05 -0500

    9-print_comb for pchr con pchr pchr empty

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,13 @@ int main(void)
 	int x;
 
 	for (x = '0'; x <= '9'; x++)
-    {
+	{
 		putchar(x);	
 		if (x == '9')
 		continue;
 		putchar(',');
 		putchar(' ');
-    }
-
+	}
 	putchar('\n');
 	return (0);
 }
