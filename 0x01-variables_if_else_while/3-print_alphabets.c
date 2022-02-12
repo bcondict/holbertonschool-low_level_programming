93375acedfcc45460d1758f731a50b1735a08ae7
bcondict <jesus.junc10@gmail.com>
2022-02-11 19:14:15 -0500

    3-print_alphabets.c same in uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - sprint alphabet in lowercase
+* Return: Always 0
+*/
+int main(void)
+{
+	char x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
