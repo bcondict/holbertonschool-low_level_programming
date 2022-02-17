8aaf23a22752417b9d75f8ca6f6d66cdfe107d3f
bcondict <jesus.junc10@gmail.com>
2022-02-17 13:26:10 -0800

    9-fizz_buzz.c complete

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - print numbers 1 to 100 with 3 and 5 condition
+ * 
+ * Return: Always 0
+ */
+int main (void)
+{
+	int a, b, c;
+
+	for (a = 1; a <= 100; a++)
+	{
+		b = a % 3;
+		c = a % 5;
+
+		if (b == 0)
+		{
+			printf("Fizz");
+
+			b = a % 5;
+
+			if (b == 0)
+				printf("Buzz");
+			printf(" ");
+		}
+		else if (c == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		}
+		else
+		{
+			printf("%d", a);
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
