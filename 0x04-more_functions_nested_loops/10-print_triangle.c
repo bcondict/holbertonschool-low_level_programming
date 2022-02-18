6fb5b9d600f6b89b0e8732438254b6295ffd3789
bcondict <jesus.junc10@gmail.com>
2022-02-17 23:49:21 -0500

    10-print_triangle.c corrections

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - print a triangle with #
+ * @size: number of # 
+ * Return: Always 0
+*/
+void print_triangle(int size)
+{
+	int spaces, numerals;
+
+	if (size <= 0)
+	_putchar('\n');
+
+	for (spaces = 1; spaces <= size; spaces++)
+	{
+		for (numerals = 1; numerals <= size; numerals++)
+		{
+			if (numerals <= (size - spaces))
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
