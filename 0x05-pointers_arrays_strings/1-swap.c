69474222bc13d40ae80d207a362783e89ad6f5f4
bcondict <jesus.junc10@gmail.com>
2022-02-21 18:50:08 -0500

    1-swap.c betty corrections

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * 
- * @a:
- * @b: 
- * 
+ * swap_int - change a and b values
+ * @a: random number
+ * @b: ramdom number 2
+ * Return: Always 0.
  */
 void swap_int(int *a, int *b)
 {
 	int y;
-    y = *a;
-    *a = *b;
-    *b = y;
+
+	y = *a;
+	*a = *b;
+	*b = y;
 }
