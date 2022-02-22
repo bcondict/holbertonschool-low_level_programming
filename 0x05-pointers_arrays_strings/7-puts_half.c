e0174b28266e821eb6a09bbfd0e07f8c7f02dc8b
bcondict <jesus.junc10@gmail.com>
2022-02-21 22:57:16 -0500

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,24 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * puts_half - print the half string
+ * @str: string
+ * Return: Always 0
  */
 void puts_half(char *str)
 {
-	
+	int a, b, c;
+
+	c = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		c++;
+
+	b = (c / 2);
+
+	if ((c % 2) == 1)
+		b = ((c + 1) / 2);
+
+	for (a = b; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
 }
