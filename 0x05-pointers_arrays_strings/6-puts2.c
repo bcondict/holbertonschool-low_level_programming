43b2233d378ff5b2173f5fa461961cc03c934389
bcondict <jesus.junc10@gmail.com>
2022-02-21 22:45:39 -0500

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,19 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * puts - print other character of a string
+ * @str: string name
+ * Return: Always 0.
  */
 void puts(char *str)
 {
-	
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if ((a % 2) == 0)
+			_putchar (str[a]);
+		else
+			continue;	
+	}
+	_putchar('\n');
 }
