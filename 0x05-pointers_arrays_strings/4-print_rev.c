b2859d42ed3c2536c9c5fa939db98f23a4f4716e
bcondict <jesus.junc10@gmail.com>
2022-02-21 22:34:01 -0500

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,21 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * print_rev - print string in reverse
+ * @s: string
+ * Return: Always 0.
  */
 void print_rev(char *s)
 {
-	
-}
+	int a, b, c = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		c++;
+	}
+
+	for (b = (c - 1); b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+	}
