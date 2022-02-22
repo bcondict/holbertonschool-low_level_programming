1060c50cc9249d28b59505f1a18e34aca6675fd4
bcondict <jesus.junc10@gmail.com>
2022-02-21 22:39:41 -0500

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * rev_string - reverse a string
+ * @s: string
+ * Return: Always 0.
  */
 void rev_string(char *s)
 {
-	
+	char c = s[0];
+	int a, b = 0;
+
+	while (s[b] != '\0')
+		b++;
+
+	for (a = 0; a < b; a++)
+	{
+		b--;
+		c = s[a];
+		s[a] = s[b];
+		s[b] = c;
+	}
 }
