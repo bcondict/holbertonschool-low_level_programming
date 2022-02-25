7eab628a76749bb523c5ff2bdb6753c3783f7fd1
bcondict <jesus.junc10@gmail.com>
2022-02-24 21:52:46 -0500

    4-rev_array.c betty corrections

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,13 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i = 0, save;
+	int i = 0, save;
 
-    n--;
-    while (i <= n)
-    {
-        save = a[i];
-        a[i++] = a[n];
-        a[n--] = save;
-    }    
+	n--;
+	while (i <= n)
+	{
+		save = a[i];
+		a[i++] = a[n];
+		a[n--] = save;
+	}
 }
