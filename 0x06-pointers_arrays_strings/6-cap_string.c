2677bc65591f325747cda201034398ad7b4b6979
bcondict <jesus.junc10@gmail.com>
2022-02-25 09:56:19 -0500

    6-cap_string.c complete

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,16 +1,22 @@
 #include "main.h"
 /**
- * @brief 
- * 
+ * cap_string - capitalizes all words
+ * @n: pointer of array
+ * Return: pointer n
  */
 char *cap_string(char *n)
 {
 	int a;
+
 	for (a = 0; n[a] != '\0'; a++)
 	{
-		if (n[a] = " " || n[a] = "	" || n[a] = "\n" || n[a] = "," || n[a] = "." || n[a] = ";" || n[a] = "!" || n[a] = "?" || n[a] = "\"" || n[a] = "(" || n[a] = ")" || n[a] = "{" || n[a] = "}" )
+		if (n[a] == ' ' || n[a] == '\t' || n[a] == '\n' || n[a] == ','
+		 || n[a] == '.' || n[a] == ';' || n[a] == '!' || n[a] == '?' || n[a] == '\"'
+		 || n[a] == '(' || n[a] == ')' || n[a] == '{' || n[a] == '}')
 		{
-			n[a++] = n[a++] + 32;
+			if (n[a + 1] >= 97 && n[a + 1] <= 122)
+				n[a + 1] = n[a + 1] - 32;
 		}
 	}
+	return (n);
 }
