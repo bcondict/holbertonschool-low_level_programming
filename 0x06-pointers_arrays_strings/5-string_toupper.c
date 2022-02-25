729e9a812b6128190922a1d0ca3575a13fedf8ec
bcondict <jesus.junc10@gmail.com>
2022-02-25 09:12:27 -0500

    5-string_toupper.c betty corrections

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,9 @@ char *string_toupper(char *n)
 	int a;
 
 	for (a = 0; n[a] != '\0'; a++)
-		{
-			if (n[a] >= 97 && n[a] <= 122)
-				n[a] = n[a] - 32;
-		}
+	{
+		if (n[a] >= 97 && n[a] <= 122)
+			n[a] = n[a] - 32;
+	}
 	return (n);
 }
