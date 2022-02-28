f382bfdb9b7e2b53d30f82b08237b47c5ec263f7
bcondict <jesus.junc10@gmail.com>
2022-02-28 18:47:06 -0500

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+# include "main.h"
+/**
+ * _strchr - return pointer to the first occurrence of c character
+ * @s: string to evaluate
+ * @c: character to found
+ * Return: string begining c character
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int a = 0;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+			return (s + a);
+		else if (s[a] != c)
+			continue;
+		else
+			return ('\0');
+	}
+}
