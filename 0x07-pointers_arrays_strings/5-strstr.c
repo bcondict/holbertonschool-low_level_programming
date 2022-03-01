a6216742a2d574059a9d9dacac216d4011bacc07
bcondict <jesus.junc10@gmail.com>
2022-02-28 21:01:42 -0500

    5-strstr.c forget it

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,16 +9,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	unsigned int a, b;
 
-	if (*haystack != '\0')
+	for (b = 0; needle[b] != '\0'; b++)
 	{
-		for (b = 0; needle[b] != '\0'; b++)
+		for (a = 0; haystack[a] != '\0'; a++)
 		{
-			for (a = 0; haystack[a] != '\0'; a++)
-			{
-				if (needle[b] == haystack[a])
-					return (haystack + a);
-			}
+			if (needle[b] == haystack[a])
+				return (haystack + a);
 		}
-		return ('\0');
 	}
+	return ('\0');
 }
