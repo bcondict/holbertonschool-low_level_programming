8d3345e623bd7b20c6d3f537be3e18d9b960f251
bcondict <jesus.junc10@gmail.com>
2022-02-28 21:16:38 -0500

    5-strstr.c betty corrections again

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,16 +8,17 @@
 char *_strstr(char *haystack, char *needle)
 {
 	unsigned int a = 0, b;
+
 	while (haystack[a] != '\0')
 	{
 		for (b = 0; needle[b] != '\0'; b++)
 		{
-			if (needle[b] != haystack[a + b] )
+			if (needle[b] != haystack[a + b])
 				break;
 		}
 		if (needle[b] == '\0')
-			return (haystack + a );
+			return (haystack + a);
 		a++;
 	}
-	return('\0');
+	return ('\0');
 }
