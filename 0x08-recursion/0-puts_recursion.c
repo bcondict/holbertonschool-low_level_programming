e67598bec7aa35ca90f60ce19bcc2f74dea17e4a
bcondict <jesus.junc10@gmail.com>
2022-03-02 18:06:43 -0500

    0-puts_recursion.c corections

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,13 @@
  * @s: string to print
  * Return: 0 if is NULL or same _puts_recursion if is not
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (!s || !*s)
 	{
-		_putchar('\n')
+		_putchar('\n');
 		return (0);
 	}
-	_putchar(*s)
+	_putchar(*s);
 	return (_puts_recursion(s + 1));
 }
