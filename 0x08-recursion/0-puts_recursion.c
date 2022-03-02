c4ff8e633c65d51b2b913e7fd71a0a62eb7d1f90
bcondict <jesus.junc10@gmail.com>
2022-03-02 18:03:41 -0500

    all files and 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ * Return: 0 if is NULL or same _puts_recursion if is not
+ */
+void _puts_recursion(char *s);
+{
+	if (!s || !*s)
+	{
+		_putchar('\n')
+		return (0);
+	}
+	_putchar(*s)
+	return (_puts_recursion(s + 1));
+}
