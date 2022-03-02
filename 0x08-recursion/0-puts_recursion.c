d37ffc7f2c8f2f9dab2c635e56af21e623146e63
bcondict <jesus.junc10@gmail.com>
2022-03-02 18:41:07 -0500

    1-print_rev_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,6 @@
 /**
  * _puts_recursion - prints a string followed by a new line
  * @s: string to print
- * Return: 0 if is NULL or same _puts_recursion if is not
  */
 void _puts_recursion(char *s)
 {
