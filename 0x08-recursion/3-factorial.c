b98b01eecaf9f11bda7d880313027206dbe43d99
bcondict <jesus.junc10@gmail.com>
2022-03-02 19:20:27 -0500

    3-factorial.c betty corrections

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * @brief 
- * 
- * 
+ * factorial - gives the factorial of a given number
+ * @n: number to takes the factorial
+ * Return: -1 if is less than 0, 1 to 0 and 1, and factorial of number
  */
 int factorial(int n)
 {
