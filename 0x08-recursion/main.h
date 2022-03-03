f6e63c57f730db5255f2348378d59842e6137160
bcondict <jesus.junc10@gmail.com>
2022-03-02 23:57:26 -0500

    6-is_prime_number.c corrections .2

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int Pow_sqrt(int a, int n);
 int is_prime_number(int n);
+int prove_of_prim(int a, int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
