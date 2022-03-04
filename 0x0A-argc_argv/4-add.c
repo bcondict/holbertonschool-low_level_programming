e8006deb8e554741c52a446bb272a0fe2a13e4dd
bcondict <jesus.junc10@gmail.com>
2022-03-04 15:49:54 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: count of argv string
+ * @argv: string
+ * Return: 1 if is error, argv hor
+ */
+int main (int argc, char *argv[])
+{
+	int hor, ver, sum = 0;
+
+	for (hor = 0, hor < argc; hor++)
+	{
+		for (ver = 1, argv[hor][ver] , ver++)
+		{
+			if (argv [hor][ver] < '0' || argv[hor][ver] > '9', ver++)
+			{
+				printf("Error\n");
+				return(1);
+			}
+		}
+
+		sum += atoi(argv[hor]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
