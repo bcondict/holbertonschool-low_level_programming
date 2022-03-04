bd7125e5791d320256dabf5727b35ba3ee5ab2a2
bcondict <jesus.junc10@gmail.com>
2022-03-04 15:04:50 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+
+ * main - print number of arguments
+
+ * @argc: count of argv
+
+ * @argv: count of argc
+
+ * Return: Always 0
+
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+	
+{
+	
+	printf("%d\n", argc - 1);
+	
+	return (0);
+	
+}
