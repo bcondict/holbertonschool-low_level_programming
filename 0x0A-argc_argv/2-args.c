4a976a604f4f7fd6f66601ec6c3e447ebc789f4a
bcondict <jesus.junc10@gmail.com>
2022-03-04 15:14:32 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments
+ * @argc: count of argv
+ * @argv: count of argc
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+		printf("%s\n", argc[a]);
+	return (0);
+}
