9b205a440fe1bdbbfdd4b1b239c18ed5f8a675d1
bcondict <98342185+bcondict@users.noreply.github.com>
2022-03-04 17:41:40 -0500

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
  * @argv: count of argc
  * Return: Always 0
  */
-int main(int arcg, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
