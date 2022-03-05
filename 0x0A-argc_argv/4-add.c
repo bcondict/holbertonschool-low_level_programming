d5c4fbed15e4c1f3aa2403804e3b818dbac8ebc6
bcondict <jesus.junc10@gmail.com>
2022-03-04 23:30:27 -0500

    4-add.cfull corrections

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,18 +6,18 @@
  * @argv: string
  * Return: 1 if is error, argv hor
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int hor, ver, sum = 0;
 
 	for (hor = 0, hor < argc; hor++)
 	{
-		for (ver = 1, argv[hor][ver] , ver++)
+		for (ver = 1, argv[hor][ver], ver++)
 		{
-			if (argv [hor][ver] < '0' || argv[hor][ver] > '9', ver++)
+			if (argv[hor][ver] < '0' || argv[hor][ver] > '9', ver++)
 			{
 				printf("Error\n");
-				return(1);
+				return (1);
 			}
 		}
 
