d6805414ce5637996466d19e0503b127c3f510e8
bcondict <jesus.junc10@gmail.com>
2022-03-08 23:07:09 -0500

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicate str string and goes toa newly
+ * @str: source string
+ * Return: null if null, copy string + 1 if not
+ */
+char *_strdup(char *str)
+{
+	char *copy;
+	int a, counter = 0;
+
+	/* if string is null return null */
+	if (str == NULL)
+		return (NULL);
+
+	/* run the source string */
+	for (a = 0; str[a]; a++)
+		counter++;
+
+	/* gives enought space for copy string + 1 space */
+	copy = malloc(sizeof(char) * (counter + 1))
+
+	if (copy == NULL)
+		return (NULL);
+
+	/* run the source string and give the same value to copy string*/
+	for (a = 0; str[a]; a++)
+		copy[a] = str[a];
+
+	copy[a] = NULL;
+
+	return (copy);
+}
