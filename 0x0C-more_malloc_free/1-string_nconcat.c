01ca578ee308a69866bce9472d1f2c6cc05cd8b9
bcondict <jesus.junc10@gmail.com>
2022-03-11 10:35:28 -0500

    1-string_nconcat.c corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,6 +27,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	conc_st = malloc(sizeof(char) * (j + k + 1));
 
+	if (!s)
+        return ('\0');
 	for (i = 0; i < j; i++)
 		conc_st[i] = s1[i];
 	for (i = 0; i < k; i++)
