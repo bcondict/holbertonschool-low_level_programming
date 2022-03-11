cfd99826d234ca605920c642564e5245d8048975
bcondict <jesus.junc10@gmail.com>
2022-03-11 10:49:54 -0500

    3-array_range.c betty

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * array_range - 
+ * array_range - arrays of integers
  * @min: number to star count
  * @max: number to finish count
  * Return: return string with value with min to max
  */
 int *array_range(int min, int max)
 {
-    int *s;
-    int i;
+	int *s;
+	int i;
 
-    if (min > max)
-        return (0);
-    
-    s = (int *)malloc(sizeof(int) * (max - min) + 1);
-    
-    for (i = min; i <= max; i++)
-        s[i] = i;
+	if (min > max)
+		return (0);
 
-    return (s);
+	s = (int *)malloc(sizeof(int) * (max - min) + 1);
+
+	for (i = min; i <= max; i++)
+		s[i] = i;
+
+	return (s);
 }
