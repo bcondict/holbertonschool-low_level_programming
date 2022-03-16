d41d30650d248715ce1562f7ac9a9c668c647b70
bcondict <jesus.junc10@gmail.com>
2022-03-15 22:47:50 -0500

    2-print_dog.c and deletions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,20 +7,17 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
-		printf("\n");
 		return;
-	}
 	if (d->name == NULL)
-		printf("nil");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == 0)
-		printf("nil");
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
-		printf("nil");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
