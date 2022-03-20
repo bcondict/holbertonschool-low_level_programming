82c003af688d42bf7379f143fe39946108ad1a20
bcondict <jesus.junc10@gmail.com>
2022-03-19 21:03:20 -0500

    2-print_strings.c correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,25 +1,26 @@
 #include "variadic_functions.h"
 /**
- * print_string - print a string inside other string
+ * print_strings - print a string inside other string
+ * @separator: string between numbers
  * @n: amount of number
  * Return: Always 0.
  */
-void print_string(const char *separator. const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	va_list s_complete;
+	unsigned int i = 0;
 	char *new_string;
+	va_list s_complete;
 
 	va_start(s_complete, n);
 
 	for (; i < n; i++)
 	{
-		s_complete = va_arg(new_string, char *);
+		new_string = va_arg(s_complete, char *);
 
-		if (new_string == NULL)
+		if (s_complete == NULL)
 			printf("(nil)");
 		else
-			printf("%s", s_complete);
+			printf("%s", new_string);
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
