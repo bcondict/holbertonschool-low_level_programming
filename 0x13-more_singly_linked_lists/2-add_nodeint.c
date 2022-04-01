5dce3404824ea91322205d66308498224d000b8b
bcondict <jesus.junc10@gmail.com>
2022-04-01 18:11:54 -0500

    2-add_nodeint.c betty corrections

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 /**
- * @brief 
- * 
+ * add_nodeint - add a new int node
+ * @head: head of node
+ * @n: integer to inster in node
+ * Return: pointer of head
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = NULL;
 
 	if (head == NULL)
-		return(NULL);
-	
+		return (NULL);
+
 	new_node = (listint_t *)malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n;
+	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
 
