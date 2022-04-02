ea69ef18d3349196f6d9115a6e6a748f36aa5a53
bcondict <jesus.junc10@gmail.com>
2022-04-01 23:24:55 -0500

    7-get_nodeint.c complete betty corrections

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,15 +2,15 @@
 /**
  * get_nodeint_at_index - get the node of a linked list
  * @head: current node
- * @index: index of node 
+ * @index: index of node
  * Return: node of index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    if (head == NULL)
-        return (NULL);
-    if (index == 0)
-        return (head);
+	if (head == NULL)
+		return (NULL);
+	if (index == 0)
+		return (head);
 
-    return (get_nodeint_at_index(head->next, index - 1));
+	return (get_nodeint_at_index(head->next, index - 1));
 }
