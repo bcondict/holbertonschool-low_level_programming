119f780cec1866d446aca5dc494917107cf08c38
bcondict <jesus.junc10@gmail.com>
2022-04-01 21:59:59 -0500

    5-free_listint2.c complete

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- * @brief 
- * 
+ * free_listint2 - free the linked list and set head to null
+ * @head: head of nodes 
+ * Return: Always 0 (success)
  */
 void free_listint2(listint_t **head)
 {
 	if (*head == NULL || head == NULL)
 		return;
-	free_listint2(*head->next);
+	free_listint2(&(*head)->next);
 	free(*head);
 	*head = NULL;
-
 }
