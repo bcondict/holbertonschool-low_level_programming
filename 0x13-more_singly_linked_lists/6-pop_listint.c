937c9ca1574a9b3ac6585a8fa4131d3a0aadb511
bcondict <jesus.junc10@gmail.com>
2022-04-01 22:52:31 -0500

    6-pop_listint.c complete

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - delete head node and return his data
+ * @head: pointer of head's node
+ * Return: data of head node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *next_value = NULL;
+	int copy;
+
+	if (*head == NULL || head == NULL)
+		return (0);
+
+	copy = (*head)->n;
+	next_value = (*head)->next;
+	free(*head);
+	*head = next_value;
+
+	return (n);
+}
