81c83422b19ed9d930040b84e119892493b247e5
bcondict <jesus.junc10@gmail.com>
2022-04-04 18:14:28 -0500

    4-clear_bit.c complete .2

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- *
- *
- *
+ * clear_bit - sets a value of a bit to a 0
+ * @n: pointer set to 1
+ * @index: sbit you want to set
+ * Return: Always 1 (success)
  */
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	int num1 = 0;
 
