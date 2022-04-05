9ef165f30299474130d8f669c61a59f9d2e925a1
bcondict <jesus.junc10@gmail.com>
2022-04-05 16:58:49 -0500

    1-create_file.c changes .2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	fd = open(filename, O_CRAT | O_WRONLY | O_TRUNC, 0600);	
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);	
 	if (fd == -1)
 		return (-1);
 	while (text_content[counter])
