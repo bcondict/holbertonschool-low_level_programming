f14631891f534c06b2d51e472a98753491d29edb
bcondict <jesus.junc10@gmail.com>
2022-04-05 17:01:58 -0500

    1-create_file.c complete

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content == NULL)
 	{
 		fd = open(filename, O_CREAT, 0600);
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);	
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[counter])
@@ -31,9 +31,9 @@ int create_file(const char *filename, char *text_content)
 	write_out = write(fd, text_content, counter);
 	if (write_out == -1)
 	{
-		close (fd);
+		close(fd);
 		return (-1);
 	}
 	close(fd);
-	return(1);	
+	return (1);
 }
