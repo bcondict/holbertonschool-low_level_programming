36de9a912dc563fac78a7dc525179f6ad5cab4a5
bcondict <jesus.junc10@gmail.com>
2022-05-11 15:43:18 -0500

    2-add_dnodeint.c and 0 corrections

--- 0x17-double_linked_list/0-print_dlistint.c
+++ 0x17-double_linked_list/0-print_dlistint.c
@@ -6,17 +6,15 @@
 *
 * Return: number of nodes
 */
- 
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
-	if (h == NULL && *h == NULL)
-		return(NULL);
-	for (i = 0, h, i++)
+	for (i = 0; h; i++)
 	{
-		printf("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return(i);
+	return (i);
 }
