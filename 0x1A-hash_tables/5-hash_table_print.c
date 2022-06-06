b2ce2f602fd104b49daa1366d5102e8f3bd394fa
Jesus <jesus.junc10@gmail.com>
2022-06-06 13:42:42 -0500

    5-hash_table_print.c style correction

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,34 +1,33 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_get - retrieves a value associated with a key
+* hash_table_print - retrieves a value associated with a key
 * @ht: existent hash table
-* @key: key to generate an index
 *
-* Return: the key on success or NULL if it failed
+* Return: always nothing
 */
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i = 0;
-    hash_node_t *current = NULL;
-    int count_node = 0;
+	unsigned long int i = 0;
+	hash_node_t *current = NULL;
+	int count_node = 0;
 
-    if (!ht)
-        return;
+	if (!ht)
+		return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while (current)
-        {
-            if (count_node > 0)
-                printf(", ");
-            printf("'%s': '%s'", current->key, current->value);
-            count_node++;
-            current = current->next;
-        }
-    }
-    printf("}\n");
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			if (count_node > 0)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			count_node++;
+			current = current->next;
+		}
+	}
+	printf("}\n");
 }
