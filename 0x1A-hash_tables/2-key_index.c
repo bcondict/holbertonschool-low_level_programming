0cd0004a2bbc00212b83d629e298396ea4decbd5
Jesus <jesus.junc10@gmail.com>
2022-06-06 03:16:34 -0500

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,16 @@
 #include "hash_tables.h"
 
 /**
+* key_index - gives a index of a key
+* @key: key to generate a index
+* @size: size of array of hash table
 *
-*
-*
-*
-*
+* Return: index of the key
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    if (key == NULL)
+    if (key == NULL || *key == NULL || size == 0)
+        return (0);
+    return (hash_djb2(key) % size);
 }
