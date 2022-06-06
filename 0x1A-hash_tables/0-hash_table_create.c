a3ed2cdd524c0fbe587af3e62f96a884ca3b24dc
Jesus <jesus.junc10@gmail.com>
2022-06-06 03:12:19 -0500

    0-hash_table_create.c style correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,11 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL; 
+	hash_table_t *new_table = NULL;
 	hash_node_t **new_index = NULL;
 
 	if (size <= 0)
-		return(NULL);
+		return (NULL);
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
