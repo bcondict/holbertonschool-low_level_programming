bdc6e7741ee96152490f256c51708ca2d14543d0
Jesus <jesus.junc10@gmail.com>
2022-06-06 13:27:44 -0500

    4-hash_table_get.c correction style

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,7 @@
 * @ht: existent hash table
 * @key: key to generate an index
 *
-* Return: 
+* Return: the key on success or NULL if it failed
 */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -13,18 +13,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *current = NULL;
 	unsigned long int index = 0;
 
-    if (!ht)
+	if (!ht)
 		return (NULL);
-    
-	index = key_index((unsigned char *)key, ht->size);
-    if (ht->array[index])
-    {
-        current = ht->array[index];
-        while (current && strcmp(current->key, key) != 0)
-            current = current->next;
-    }
-    if (!current)
-        return(NULL);
 
-    return (current->value);
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index])
+	{
+		current = ht->array[index];
+		while (current && strcmp(current->key, key) != 0)
+			current = current->next;
+	}
+	if (!current)
+		return (NULL);
+
+	return (current->value);
 }
