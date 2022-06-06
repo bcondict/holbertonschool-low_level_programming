88f99f2b67204dcc317847d6dcc3b1d96d09e6ff
Jesus <jesus.junc10@gmail.com>
2022-06-06 03:11:13 -0500

    0-hash_table_create.c complete

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,24 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL, **new_index = NULL;
+	hash_table_t *new_table = NULL; 
+	hash_node_t **new_index = NULL;
+
+	if (size <= 0)
+		return(NULL);
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
-	new_index = calloc(sizeof(hash_node_t *));
+	new_index = calloc(size, sizeof(hash_node_t *));
 	if (new_index == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
 
-	new_table = size;
 	new_table->array = new_index;
+	new_table->size = size;
 
 	return (new_table);
 }
