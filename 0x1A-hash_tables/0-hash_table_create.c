49c6216b5c9577157268747039370bb717df0eae
Jesus <jesus.junc10@gmail.com>
2022-06-06 03:04:41 -0500

    0-hash_table_create.c correction complete

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = NULL, new_index = NULL;
+	hash_table_t *new_table = NULL, **new_index = NULL;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
