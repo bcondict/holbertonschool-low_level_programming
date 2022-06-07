f30321b142e58eca687f386cb92c94c43cad9e3f
Jesus <jesus.junc10@gmail.com>
2022-06-06 23:57:47 -0500

    6-hash_table_delete.c new test

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -12,18 +12,21 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i = 0;
 	hash_node_t *current = NULL, *tmp = NULL;
 
-	if (!ht || !ht->array || !ht->size == 0)
+	if (!ht)
 		return;
 	for (i = 0; i < ht->size; i++)
 	{
-		current = ht->array[i];
-		while (current)
+		if (ht->array[i] != NULL)
 		{
-			tmp = current->next;
-			free(current->key);
-			free(current->value);
-			free(current);
-			current = tmp;
+			current = ht->array[i];
+			while (current)
+			{
+				tmp = current->next;
+				free(current->key);
+				free(current->value);
+				free(current);
+				current = tmp;
+			}
 		}
 	}
 	free(ht->array);
